e5f934baeb41cc4c200eb6e7aac071a96b22f805
mdestro1997 <44907126+mdestro1997@users.noreply.github.com>
2021-06-29 11:36:46 +0200

    Add files via upload

--- demo/src/main/java/com/example/demo/ApiController/UserService.java
+++ demo/src/main/java/com/example/demo/ApiController/UserService.java
@@ -6,8 +6,7 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     public User newUser(String UserType, Double cost) {
-        // UserType userType;
-        // String type = UserType.EMPLOYEE.getUserTypeId();
+       
         System.out.println(UserType);
 
         switch (UserType) {
@@ -33,22 +32,13 @@ public class UserService {
         Double discountAmount = newUser.getCost();
         Double discountPercentage = newUser.getUserType().getDiscountPercentage();
         discountAmount = discountAmount * discountPercentage;
-        // System.out.println("DISCOU"+discountAmount);
-
         newUser.setUserTypeDiscount((newUser.getCost() - discountAmount));
-        // System.out.println("TYPE"+newUser.getUserTypeDiscount());
-
         return newUser.getUserTypeDiscount();
     }
 
     public Double finalDiscount(User newUser) {
-        // Decrease 5 for each 100
         Double finaDiscount = newUser.getUserTypeDiscount() - (Math.floor(Math.floor(newUser.getCost()) / 100) * 5);
-        // System.out.println("Bill Amount After Final Discount is : " +
-        // userBill.getFinalbillCost());
-        newUser.setFinalDiscount(finaDiscount);
-        // System.out.println(newUser.getFinalDiscount());
-
+        newUser.setFinalDiscount(finaDiscount); 
         return newUser.getFinalDiscount();
     }
 }
